b7e51fecf72c8d4efebb4675ed2d7192cb6a88a5
thahathafani <thahafazzin@gmail.com>
2025-01-21 21:12:34 +0530

    folder stracture + Splash screen + On Boarding Pages

---
+++ android/app/src/main/kotlin/com/example/mobapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:mobapp/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
+import 'package:mobapp/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
+import 'package:mobapp/utils/constants/image_strings.dart';
+import 'package:mobapp/utils/constants/sizes.dart';
+import 'package:mobapp/utils/constants/text_strings.dart';
+import 'package:mobapp/utils/helpers/helper_functions.dart';
+
+import '../../../../utils/device/device_utility.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          /// Horizontal Scrollable Pages
+          PageView(
+            children: const [
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,),
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,),
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,),
+            ],
+
+          ),
+
+          /// Skip Button
+          const OnBoardingSkip(),
+
+          /// Dot Navigation Smooth Page Indicator
+        ],
+      ),
+    );
+  } 
+}
+
