53a6e0f210cab5edc3bccd31d5147f8c28f310c8
thahathafani <thahafazzin@gmail.com>
2025-01-22 16:07:58 +0530

    On boarding merged with color updates

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mobapp/features/authentication/controllers/onboarding_controllers.dart';
+import 'package:mobapp/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart';
 import'package:mobapp/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
 import 'package:mobapp/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
 import 'package:mobapp/utils/constants/colors.dart';
@@ -6,8 +9,7 @@ import 'package:mobapp/utils/constants/image_strings.dart';
 import 'package:mobapp/utils/constants/sizes.dart';
 import 'package:mobapp/utils/constants/text_strings.dart';
 import 'package:mobapp/utils/helpers/helper_functions.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
+import 'package:iconsax/iconsax.dart';
 import '../../../../utils/device/device_utility.dart';
 
 class OnBoardingScreen extends StatelessWidget {
@@ -15,11 +17,15 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(OnBoardingController());
+
     return Scaffold(
       body: Stack(
         children: [
           /// Horizontal Scrollable Pages
           PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
             children: const [
               OnBoardingPage(
                 image: TImages.onBoardingImage1,
@@ -41,7 +47,10 @@ class OnBoardingScreen extends StatelessWidget {
           const OnBoardingSkip(),
 
           /// Dot Navigation Smooth Page Indicator
-          const OnBoardingDotNavigation()
+          const OnBoardingDotNavigation(),
+          
+          /// Circular Button
+          const OnBoardingNextButton(),
 
         ],
       ),
@@ -49,24 +58,27 @@ class OnBoardingScreen extends StatelessWidget {
   }
 }
 
-class OnBoardingDotNavigation extends StatelessWidget {
-  const OnBoardingDotNavigation({
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
+
     return Positioned(
-      bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
-      left: TSizes.defaultSpace,
-      child: SmoothPageIndicator(
-        controller: PageController(),
-        count: 3,
-        effect: ExpandingDotsEffect(activeDotColor: dark ? TColors.light: TColors.dark, dotWidth: 8, dotHeight: 8),
+      right: TSizes.defaultSpace,
+      bottom: TDeviceUtils.getBottomNavigationBarHeight(),
+      child: ElevatedButton(
+        onPressed: (){},
+        style: ElevatedButton.styleFrom(shape: const CircleBorder(), backgroundColor: dark ? TColors.primary: Colors.black),
+        child: const Icon(Iconsax.arrow_right_3),
       ),
     );
   }
 }
+
+
 
 
