bec8769e6e3594679d31fbbdc72dd3199230e103
thahathafani <thahafazzin@gmail.com>
2025-01-23 18:45:23 +0530

    signup page

---
+++ lib/features/authentication/screens/signup widgets/signup.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mobapp/features/authentication/screens/signup%20widgets/signup_form.dart';
+import 'package:mobapp/utils/constants/text_strings.dart';
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../common/widgets/form_divider.dart';
+import '../../../../common/widgets/social_buttons.dart';
+import '../login/login.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+          actions: [
+          IconButton(onPressed: () => Get.offAll(() => const LoginScreen()), icon: const Icon (CupertinoIcons.clear))
+    ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Title
+              Text(
+                TTexts.signupTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Form
+              const TSignupForm(),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Divider
+              TFormDivider(dividerText: TTexts.orSignUpWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Social Buttons
+              const TSocialButtons(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
