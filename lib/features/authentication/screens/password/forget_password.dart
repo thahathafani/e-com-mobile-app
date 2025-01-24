4c5bed902f980564f1625f50d8cef8fff3a19984
thahathafani <thahafazzin@gmail.com>
2025-01-24 20:15:24 +0530

    forget Password page added

---
+++ lib/features/authentication/screens/password/forget_password.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mobapp/features/authentication/screens/password/reset_password.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/constants/text_strings.dart';
+import '../Login/Login.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () => Get.to(() => const LoginScreen()),
+            icon: const Icon(CupertinoIcons.clear),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// Headings
+            Text(
+              TTexts.forgetPasswordTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems),
+            Text(
+              TTexts.forgetPasswordSubTitle,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections * 2),
+
+            /// Text Field
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: TTexts.email,
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            /// Submit Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => Get.off(() => const ResetPassword()),
+                child: const Text(TTexts.submit),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
