ceb37f1687f345a1f4a5640f87c937c6b7edcd2d
thahathafani <thahafazzin@gmail.com>
2025-01-25 17:49:49 +0530

    categories widgets

---
+++ lib/common/widgets/image_text_widgets/vertical_image_text.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:mobapp/utils/constants/colors.dart';
+import 'package:mobapp/utils/constants/sizes.dart';
+import 'package:mobapp/utils/helpers/helper_functions.dart';
+
+
+class TVerticalImageText extends StatelessWidget {
+  const TVerticalImageText({
+    super.key,
+    required this.image,
+    required this.title,
+    this.textColor = TColors.white,
+    this.backgroundColor = Colors.white,
+    this.onTap,
+  });
+
+  final String image;
+  final String title;
+  final Color textColor;
+  final Color? backgroundColor;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(right: TSizes.spaceBtwItems),
+        child: Column(
+          children: [
+            // Circular container with image
+            Container(
+              width: 50,
+              height: 50,
+              padding: const EdgeInsets.all(TSizes.sm),
+              decoration: BoxDecoration(
+                color: backgroundColor ?? (dark ? TColors.black : TColors.white),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(
+                child: Image(
+                  image: AssetImage(image),
+                  fit: BoxFit.cover,
+                  color: dark ? TColors.light : TColors.dark,
+                ),
+              ),
+            ),
+            // Text below the circular image
+            const SizedBox(height: TSizes.spaceBtwItems / 2),
+            SizedBox(
+              width: 55,
+              child: Text(
+                title,
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium
+                    ?.apply(color: textColor),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
