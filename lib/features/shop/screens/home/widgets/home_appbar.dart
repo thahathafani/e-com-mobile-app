04a7caba9d1ad906e4ac726b8b97d191d989d881
thahathafani <thahafazzin@gmail.com>
2025-01-25 16:31:25 +0530

    Appbar for home by widgets

---
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../common/widgets/appbar/appbar.dart';
+import '../../../../../common/widgets/products_cart/cart_menu_icon.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/text_strings.dart';
+
+class THomeAppbar extends StatelessWidget {
+  const THomeAppbar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TAppBar(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            TTexts.homeAppbarTitle,
+            style: Theme.of(context)
+                .textTheme
+                .labelMedium!
+                .apply(color: TColors.grey),
+          ),
+          Text(
+            TTexts.homeAppbarSubTitle,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .apply(color: TColors.white),
+          ), // Text
+        ],
+      ), // Column
+      actions: [
+        TCartCounterIcon(
+          onPressed: () {},
+          iconColor: TColors.white,
+        ),
+      ],
+    );
+  }
+}
